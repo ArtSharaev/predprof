c6a86730a72a5c6e0bb33280276c7e20bda391c8
ArtSharaev <sharaev_00@inbox.ru>
2024-03-30 09:42:13 +0300

    N1

---
+++ 1.py
@@ -0,0 +1,18 @@
+import datetime as dt
+
+with open("songs.csv", "r", encoding="utf-8") as f:
+    songs = [x.strip().split(";") for x in f.readlines()[1:]]
+check_date = dt.datetime.fromisoformat("2002-01-01")  # дата фильтрации
+obr_date = dt.datetime.fromisoformat("2023-05-12")  # дата обращения
+new_songs = []
+for s in songs:
+    song = s.copy()
+    date = dt.datetime.fromisoformat("-".join(song[-1].split(".")[::-1]))  # перевод даты в тип datetime
+    if date < check_date:  # фильтрация по году
+        if song[0] == "0":  # если нет данных о прослушиваниях
+            dlt = obr_date - date
+            song[0] = str(int(abs(dlt.days / (len(song[1]) + len(song[2]))) * 10000))
+        new_songs.append(song)
+with open("songs_new.csv", "w", encoding="utf-8") as f:
+    for song in new_songs:
+        print(";".join(song), file=f)

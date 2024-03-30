005f06a631f9deb83c6c55e49487995cebdda4e3
ArtSharaev <sharaev_00@inbox.ru>
2024-03-30 10:39:50 +0300

    N1 changes

--- 1.py
+++ 1.py
@@ -8,11 +8,13 @@ new_songs = []
 for s in songs:
     song = s.copy()
     date = dt.datetime.fromisoformat("-".join(song[-1].split(".")[::-1]))  # перевод даты в тип datetime
-    if date < check_date:  # фильтрация по году
-        if song[0] == "0":  # если нет данных о прослушиваниях
-            dlt = obr_date - date
-            song[0] = str(int(abs(dlt.days / (len(song[1]) + len(song[2]))) * 10000))
-        new_songs.append(song)
+    if date < check_date:  # вывод песен по дате выхода не позже 01.01.2002
+        print(f"{song[2]} - {song[1]} - {song[0]}")
+    if song[0] == "0":  # если нет данных о прослушиваниях
+        dlt = obr_date - date
+        song[0] = str(int(abs(dlt.days / (len(song[1]) + len(song[2]))) * 10000))
+    new_songs.append(song)
 with open("songs_new.csv", "w", encoding="utf-8") as f:
+    print("streams;artist_name;track_name;date", file=f)
     for song in new_songs:
         print(";".join(song), file=f)
